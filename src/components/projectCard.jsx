fc3a606a7cf72d29fc30dca21a09da2d0818f692
sebastian.britos <sebastian.britos.peluffo@gmail.com>
2024-09-08 21:33:53 -0300

    otro cambio

--- src/components/projectCard.jsx
+++ src/components/projectCard.jsx
@@ -5,8 +5,8 @@ import { Button } from '../components/ui/button';
 function ProjectCard({ img = '/projectphoto.jpg', desc = 'Coming Soon', link = 'https://www.youtube.com/watch?v=yqWX86uT5jM' }) {
     return (
         <div className="relative group">
-            <div className="flex justify-center items-center">
-                <div className="relative text-center rounded-md border-2 lg:transition-transform lg:duration-500 lg:ease-in-outlg:transform lg:group-hover:scale-105">
+            <div className="flex justify-center items-center ">
+                <div className="relative cursor-pointer text-center rounded-md border-2 lg:transition-transform lg:duration-500 lg:ease-in-outlg:transform lg:group-hover:scale-105">
                     <div className='overflow-hidden lg:[width:300px] lg:[height:250px] xl:[width:400px] xl:[height:280px] md:[width:400px] md:[height:280px] sm:[width:400px] sm:[height:280px] [width:250px] [height:300px]'>
                         <img
                             className="object-cover h-full w-full hover:border-none rounded-md blur-sm"
