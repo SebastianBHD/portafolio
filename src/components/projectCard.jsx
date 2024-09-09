ea84dad52d3f79dc1e48e2e0c505d444ac75795b
sebastian.britos <sebastian.britos.peluffo@gmail.com>
2024-09-08 21:26:09 -0300

    cambio imgs

--- src/components/projectCard.jsx
+++ src/components/projectCard.jsx
@@ -7,11 +7,13 @@ function ProjectCard({ img = '/projectphoto.jpg', desc = 'Coming Soon', link='ht
         <div className="relative group">
             <div className="flex justify-center items-center">
                 <div className="relative text-center rounded-md border-2 lg:transition-transform lg:duration-500 lg:ease-in-outlg:transform lg:group-hover:scale-105">
+                    <div className='overflow-hidden lg:[width:300px] lg:[height:250px] xl:[width:400px] xl:[height:280px] md:[width:400px] md:[height:280px] sm:[width:400px] sm:[height:280px] [width:250px] [height:300px]'>
                     <img
-                        className="object-cover hover:border-none rounded-md blur-sm lg:[width:300px] lg:[height:250px] xl:[width:400px] xl:[height:280px] md:[width:400px] md:[height:280px] sm:[width:400px] sm:[height:280px] [width:250px] [height:300px]"
+                        className="object-cover h-full w-full hover:border-none rounded-md blur-sm"
                         src={img}
                         alt="Developer"
                     />
+                    </div>
                     <div className="absolute inset-0 grid xl:p-10 p-5 justify-center hover:border-none rounded-md bg-secondary bg-opacity-80 opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
                         <p className="text-white xl:text-md text-sm">
                             {desc}
